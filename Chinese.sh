1ef53271fba5917a907a62b5a5db0a95e10777c5
AnonMini <49457366+AnonMini@users.noreply.github.com>
2019-04-16 09:04:43 -0500

    Create Chinese.sh

---
+++ Chinese.sh
@@ -0,0 +1,12 @@
+echo 你叫什么名字？
+read $名
+
+
+echo Hace mucho tiempo un niño llamado $名
+echo Emprendio un viaje en busca de dinero
+echo Medio Perdido y cansado, una señora le dijo que fuese con ella.
+echo llegaron a un gran palacete abandonado
+echo la mujer le dijo que podia dormir entre los sacos de una esquina
+echo tambien le dijo que "NUNCA" subiese a el siguiente piso
+echo $名 le pregunto por que pero ella ya habia subido
+echo 
